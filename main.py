703d26eac4706e890e6064b6a80ecb931ea441b3
Szymon Dziedzic <sz.dziedz@gmail.com>
2021-06-03 11:37:03 +0200

    First generator

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+from random_number_generator.generator import Generator
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+G = Generator()
 
+G_x = []
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+for i in range(1000000):
+    G_x.append(G.generate_number())
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.hist(G_x)
+plt.show()
---
+++ random_number_generator/generator.py
@@ -0,0 +1,12 @@
+class Generator:
+    def __init__(self, a=7 ** 5, m=2**31 - 1, x0=1):
+        self.a = a
+        self.m = m
+        self.last_x = x0
+        self.next_step = 1
+
+    def generate_number(self):
+        number = (self.a * self.last_x) % self.m
+        self.last_x = number
+        self.next_step = self.next_step + 1
+        return number
